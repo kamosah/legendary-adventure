bd9d0b474aa55acf2b3e4bdef2b3ed5f449ca861
kamosah <kamosah1@gmail.com>
2022-06-08 19:14:28 -0400

    Adding apollo server

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,47 @@
 import * as express from 'express';
-import { Message } from '@legendary-adventure/api-interfaces';
+import { ApolloServer, gql } from 'apollo-server-express';
+import * as http from 'http';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 
-const app = express();
+import type { Message } from '@legendary-adventure/api-interfaces';
 
 const greeting: Message = { message: 'Welcome to api!' };
 
-app.get('/api', (req, res) => {
-  res.send(greeting);
-});
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
-});
-server.on('error', console.error);
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+// express server
+const app = express();
+app.get('/api', (req, res) => res.send(greeting));
+
+async function startApolloServer(typeDefs, resolvers) {
+  const httpServer = http.createServer(app);
+
+  // apollo server
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  const port = process.env.port || 3333;
+  await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+}
+
+startApolloServer(typeDefs, resolvers);
